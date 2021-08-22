023c72be171e227e0e10bc8d3dbb08ae0183af29
OksiDavydova <oksana.acva@gmail.com>
2021-08-22 16:19:57 +0300

    add markup-info and markup-list

--- src/index.js
+++ src/index.js
@@ -1,24 +1,21 @@
 import "./sass/main.scss";
-import { error, alert } from "@pnotify/core";
+import _debounce from "lodash.debounce";
+import { error } from "@pnotify/core";
 import "@pnotify/core/dist/PNotify.css";
 import "@pnotify/core/dist/BrightTheme.css";
-import "@pnotify/confirm/dist/PNotifyConfirm.css";
 
-import articleTpl from "./template/article-tpl.hbs";
-import countriesListTpl from "./template/countries-list-tpl.hbs";
-import _debounce from "lodash.debounce";
+import getRefs from './js/refs.js';
 import { fetchCountries } from "./js/fetchCountries.js";
+import markupCountryInfo from './js/markup-info.js';
+import markupList from './js/markup-list.js';
 
-const refs = {
-  search: document.querySelector("#search"),
-  country: document.querySelector(".country"),
-};
+const refs = getRefs();
 let searchQuery = "";
 
 refs.search.addEventListener("input", _debounce(onSearchCountry, 500));
 
 function onSearchCountry() {
-  refs.country.innerHTML = ''; //clear input
+  refs.countryOverlay.innerHTML = ''; //clear input
   searchQuery = refs.search.value;
   // console.log(searchQuery);
   fetchCountries(searchQuery)
@@ -32,21 +29,11 @@ function renderCollection(countries) {
       text: "Too many matches found. Please enter a more specific query!",
     });
   }
-
   if (countries.length >= 2 && countries.length <= 10) {
     markupList(countries);
     return;
   }
-
   markupCountryInfo(countries);
 }
 
-function markupList(countries) {
-  const markupList = countriesListTpl(countries);
-  refs.country.insertAdjacentHTML("beforeend", markupList);
-}
 
-function markupCountryInfo(countries) {
-  const markupArticle = articleTpl(countries);
-  refs.country.insertAdjacentHTML("beforeend", markupArticle);
-}
